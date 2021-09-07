e48832dc39c6bac47c186966df172e538ef9933f
khoahuynhdev <khoa0319@gmail.com>
2021-09-07 17:13:40 +0700

    add ssh notify script

---
+++ login_notify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ["$PAM_TYPE" != "close_session"]; then
+  host="$(hostname)"
+  curl -d '{"user":"'$PAM_USER'", "user_host":"'$PAM_RHOST'", "host": "'$host'"}' -H "Content-Type: application/json" -X POST http://209.97.168.191:8000/ssh &
+fi
