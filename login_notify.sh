a47e81df8472f7b91bb0dd6c2e4b2e17801ada2e
khoahuynhdev <khoa0319@gmail.com>
2021-09-08 18:21:31 +0700

    typo

--- login_notify.sh
+++ login_notify.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ["$PAM_TYPE" != "close_session"]; then
+if [ "$PAM_TYPE" != "close_session" ]; then
   host="$(hostname)"
   curl -d '{"user":"'$PAM_USER'", "user_host":"'$PAM_RHOST'", "host": "'$host'"}' -H "Content-Type: application/json" -X POST http://209.97.168.191:8000/ssh &
 fi
